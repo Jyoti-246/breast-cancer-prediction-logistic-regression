dcda5c2920a5ba0b16cd50041735fdc5c93ceaf9
Jyoti-246 <jyotikedia203@gmail.com>
2026-06-23 08:35:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import streamlit as st
-import pickle
 import numpy as np
+import pickle
 
-model = pickle.load(open('breast_cancer_model.pkl', 'rb'))
+model = pickle.load(open("breast_cancer_model.pkl", "rb"))
 
 st.title("Breast Cancer Prediction")
 
-radius_mean = st.number_input("Radius Mean")
-texture_mean = st.number_input("Texture Mean")
-perimeter_mean = st.number_input("Perimeter Mean")
+ct = st.number_input("Clump Thickness", min_value=1, max_value=10)
+ucs = st.number_input("Uniformity of Cell Size", min_value=1, max_value=10)
+ucs2 = st.number_input("Uniformity of Cell Shape", min_value=1, max_value=10)
+ma = st.number_input("Marginal Adhesion", min_value=1, max_value=10)
+secs = st.number_input("Single Epithelial Cell Size", min_value=1, max_value=10)
+bn = st.number_input("Bare Nuclei", min_value=1, max_value=10)
+bc = st.number_input("Bland Chromatin", min_value=1, max_value=10)
+nn = st.number_input("Normal Nucleoli", min_value=1, max_value=10)
+m = st.number_input("Mitoses", min_value=1, max_value=10)
 
 if st.button("Predict"):
-    features = np.array([[radius_mean, texture_mean, perimeter_mean]])
+    features = np.array([[ct, ucs, ucs2, ma, secs, bn, bc, nn, m]])
     prediction = model.predict(features)
 
-    if prediction[0] == 1:
-        st.success("Malignant")
-    else:
-        st.success("Benign")
+    st.write("Prediction:", prediction[0])
